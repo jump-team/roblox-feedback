372b44683298d613ea9aac8b04d32668c391fa20
Glitch (roblox-feedback) <none>
2018-12-19 12:47:27 +0000

    🌅👱 Updated with Glitch

--- server.js
+++ server.js
@@ -19,31 +19,59 @@ let feedbackSchema = new Schema({
   date: { type: Date, default: Date.now }
 });
 let Feedback = mongoose.model('Feedback', feedbackSchema);
+const bearerToken = require('express-bearer-token');
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 db.once('open', function() {
   app.use(bodyParser.json());
+  app.use(bearerToken());
   // http://expressjs.com/en/starter/basic-routing.html
   app.get('/', function(request, response) {
     response.send("Docs at https://glitch.com/~roblox-feedback");
   });
   app.get('/feedback', function(request, response) {
     Feedback.find(function(error, feedback) {
-      response.send(feedback);
+      response.status(200).json(feedback);
     });
   });
   app.post('/feedback', function(request, response) {
-    let newFeedback = new Feedback({ subject: request.body.subject, author: request.body.author, body: request.body.body });
-    newFeedback.save();
-    response.status(200).send("Feedback added");
+    if (request.token === process.env.SECRET) {
+      let newFeedback = new Feedback({ subject: request.body.subject, author: request.body.author, body: request.body.body });
+      newFeedback.save();
+      response.status(200).send("Feedback added");
+    }else{
+      response.status(401).send("Unauthorized");
+    }
   });
   app.get('/feedback/:author', function(request, response) {
-    let author = request.params.author
+    let author = request.params.author;
     Feedback.find({ author: author }, function(error, feedback) {
       if (error) { response.status(500).send("Error"); return; }
-      response.status(200).send(feedback);
+      response.status(200).json(feedback);
     });
+  });
+  app.post("/feedback/:id/comment", function(request, response) {
+    if (request.token === process.env.SECRET) {
+      let id = request.params.id;
+      let author = request.body.author;
+      let comment = request.body.comment;
+      Feedback.findById(id, function (error, feedback) {
+        if (error) { response.status(404).send("Doccument not found"); return; }
+        if (author.toString() === process.env.APPROVED_USER || author.toString() === feedback.author) {
+          // Post feedback
+          feedback.comments.push({ body: comment, author: author });
+          feedback.save(function (error, newFeedback) {
+            if (error) { response.status(500).send("Server error"); return; }
+            response.status(200).json(newFeedback);
+          });
+        }else{
+          response.status(401).send("Unauthorized " + author + " " + process.env.APPROVED_USER);
+        }    
+      });
+    }else{
+      response.status(400).send("Unauthorized");
+    }
   });
   
   // listen for requests :)
